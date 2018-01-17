8c148d146e8db82a21b6ab81161181b89d4e1481
Claimundefine <35274216+Claimundefine@users.noreply.github.com>
2018-01-17 14:54:43 -0500

    Add files via upload

--- Display.java
+++ Display.java
@@ -1,150 +1,159 @@
 import java.util.*;
 
 public class Display {
-   Scanner sc = new Scanner(System.in);
-   String input;
-   private Player character;
-   private EventRunner events;
-   final int COURSES = 4;
-   int month = 0;
-   int num;
-   String gend;
-   String name;
-   private Type type = new Type();
-	//initialize constructor
-/*	public Display (Player chara, EventRunner events) {
-		character = chara;
-		this.events = events;
-	}*/
-	
-	//accessor and mutator for months
-   public int getMonth(){
-      return month; 
-   }
-	
-   public void addMonth(){
-      month = month+1;
-   }
-	//calls all methods
-   public void startGame() {
-      
-      System.out.println("Welcome to AYJ Simulator!");
-      this.characterCreation();
-   	//gives exposition and tips for playing game
-      System.out.println("You will be playing through various situations, selecting courses at the beginning of every year.");
-      System.out.println("Every month, you will select how much sleep, entertainment, and studying you will do.");
-      System.out.println("You will also have a chance of encountering random events that can affect your stats.");
-      System.out.println("Spend your time wisely!");
-   	//affects course selection and possible events
-      character = new Player (type.choose(num), type, gend, name, true, month);
-      character.setGrade(9);
-      this.chooseCourses();
-   	//counts time for 40 months
-      while(this.getMonth()<=40)
-      {
-         int counter = this.getMonth();
-      	//course selection after every term
-         if(counter == 6) {
-            System.out.println("First term already gone in a flash! Here's to seven more!");
-            this.chooseCourses();
-         }
-         if (counter == 16) {
-            System.out.println("You're getting the hang of this, almost at the halfway mark!");
-            this.chooseCourses();
-         }
-         if (counter == 26) {
-            System.out.println("You're pretty much an expert at this point, finish strong!");
-            this.chooseCourses();
-         }
-         if(counter == 36) {
-            System.out.println("Last semester! Make it count, the end is near.");
-            this.chooseCourses();
-         }
-      	//graduates to next grade, new events and classes to take
-         if(counter == 11) {
-            character.setGrade(10);
-            System.out.println("First year already gone by! Welcome to your sophmore year.");
-            this.chooseCourses();
-         }
-         if(counter == 21) {
-            character.setGrade(11);
-            System.out.println("Already Grade 11! Enjoy it while you can, the world is still your oyster.");
-            this.chooseCourses();
-         }
-         if(counter == 31){
-            character.setGrade(12);
-            System.out.println("Final year! Just ten short months before you enter the real world.");
-            this.chooseCourses();
-         }
-      	//summarizes what happened the last month
-         if(counter>1) {
-            int happiness = character.stats.getHappiness();
-         	//Different response depending on their happiness level
-            if(happiness>8) {
-               System.out.println("Your happiness is through the roof! Here are the rest of yours stats.");
-            }else if (happiness>4) {
-               System.out.println("Life is fine, but it could also be better! Here are the results of last month.");
-            }
-            else {
-               System.out.println("You've done enough, but at what cost? You've been depressed all month. Here are your stats.");
-            }
-         	//prints out all stats for player to see that month
-            System.out.print(character.stats);
-            for (int i = 0; i< COURSES ; i++)
-            {
-               System.out.printf("Marks this month for %s: %f%n", character.schedule[i].getSubject(), character.schedule[i].list[counter%5].getMark());
-               System.out.printf("Mark for course overall:%f%n", character.schedule[i].getCurrMark());
-            }
-         }
-      	//player chooses how to allocate time
-         System.out.println("Another month, another round of decisions. Pick how you would like to allocate your time this month.");
-         character.spendTime();
-      	//initializes event per month
-         events.rollEvent(counter);
-      	//increases month value after all course work and events are completed
-         for(int i = 0 ; i<4 ; i++) {
-            character.doTest(i);
-         }
-         this.addMonth();
-      	//Gives the player a pause
-         System.out.println("The end of another month... (Press enter to continue)");
-         input=sc.nextLine();
-      }
-   	//initialize ending after all 40 months have passed
-      this.startEnding();
-   }
-	
-	//called from start game, sets gender and character type
-   public void characterCreation()
-   {
-      String input;
-      boolean valid = false;
-   	//Sets name and gender
-      System.out.println("What is your name?");
-      name = sc.nextLine();
-   	//character.setName(input);
-      System.out.println("What is your gender?");
-      gend = sc.nextLine();
-   	//character.setGender(input);
-   	
-   	//selects Character type
-      System.out.println("What character class do you want to be? Input a number for each result.");
-      System.out.println("Charmer: 1");
-      System.out.println("Artist: 2");
-      System.out.println("Jock: 3");
-      System.out.println("Unspecialized: 4");
-      System.out.println("Nerd: 5");
-      num = sc.nextInt();
-   }
-	
-   public void chooseCourses(){
-         character.courseSelection();
-   }
-	//initialized once sufficient time has passed
-	
-	//TODO: Finish ending
-   public void startEnding(){
-   	
-   }
+	Scanner sc = new Scanner(System.in);
+	String input;
+	private Player character;
+	private EventRunner events;
+	final int COURSES = 4;
+	int month = 0;
+	int num;
+	String gend;
+	String name;
+	private Type type = new Type();
+	// initialize constructor
+	/*
+	 * public Display (Player chara, EventRunner events) { character = chara;
+	 * this.events = events; }
+	 */
+
+	// accessor and mutator for months
+	public int getMonth() {
+		return month;
+	}
+
+	public void addMonth() {
+		month = month + 1;
+	}
+
+	// calls all methods
+	public void startGame() {
+
+		System.out.println("Welcome to AYJ Simulator!");
+		this.characterCreation();
+		// gives exposition and tips for playing game
+		System.out.println(
+				"You will be playing through various situations, selecting courses at the beginning of every year.");
+		System.out.println("Every month, you will select how much sleep, entertainment, and studying you will do.");
+		System.out.println("You will also have a chance of encountering random events that can affect your stats.");
+		System.out.println("Spend your time wisely!");
+		// affects course selection and possible events
+		character = new Player(type.choose(num), type, gend, name, true, month);
+		character.setGrade(9);
+		this.chooseCourses();
+		// counts time for 40 months
+		while (this.getMonth() <= 40) {
+			int counter = this.getMonth();
+			// course selection after every term
+			if (counter == 6) {
+				System.out.println("First term already gone in a flash! Here's to seven more!");
+				this.chooseCourses();
+			}
+			if (counter == 16) {
+				System.out.println("You're getting the hang of this, almost at the halfway mark!");
+				this.chooseCourses();
+			}
+			if (counter == 26) {
+				System.out.println("You're pretty much an expert at this point, finish strong!");
+				this.chooseCourses();
+			}
+			if (counter == 36) {
+				System.out.println("Last semester! Make it count, the end is near.");
+				this.chooseCourses();
+			}
+			// graduates to next grade, new events and classes to take
+			if (counter == 11) {
+				character.setGrade(10);
+				System.out.println("First year already gone by! Welcome to your sophmore year.");
+				this.chooseCourses();
+			}
+			if (counter == 21) {
+				character.setGrade(11);
+				System.out.println("Already Grade 11! Enjoy it while you can, the world is still your oyster.");
+				this.chooseCourses();
+			}
+			if (counter == 31) {
+				character.setGrade(12);
+				System.out.println("Final year! Just ten short months before you enter the real world.");
+				this.chooseCourses();
+			}
+			// summarizes what happened the last month
+			if (counter > 1) {
+				int happiness = character.stats.getHappiness();
+				// Different response depending on their happiness level
+				if (happiness > 8) {
+					System.out.println("Your happiness is through the roof! Here are the rest of yours stats.");
+				} else if (happiness > 4) {
+					System.out
+							.println("Life is fine, but it could also be better! Here are the results of last month.");
+				} else {
+					System.out.println(
+							"You've done enough, but at what cost? You've been depressed all month. Here are your stats.");
+				}
+				// prints out all stats for player to see that month
+				System.out.print(character.stats);
+				for (int i = 0; i < COURSES; i++) {
+					System.out.printf("Marks this month for %s: %f%n", character.schedule[i].getSubject(),
+							character.schedule[i].list[counter % 5].getMark());
+					System.out.printf("Mark for course overall:%f%n", character.schedule[i].getCurrMark());
+				}
+			}
+			// player chooses how to allocate time
+			System.out.println(
+					"Another month, another round of decisions. Pick how you would like to allocate your time this month.");
+			character.spendTime();
+			// initializes event per month
+			events.rollEvent(counter);
+			// increases month value after all course work and events are completed
+			for (int i = 0; i < 4; i++) {
+				character.doTest(i);
+			}
+			this.addMonth();
+			// Gives the player a pause
+			System.out.println("The end of another month... (Press enter to continue)");
+			input = sc.nextLine();
+		}
+		// initialize ending after all 40 months have passed
+		this.startEnding();
+	}
+
+	// called from start game, sets gender and character type
+	public void characterCreation() {
+		String input;
+		boolean valid = false;
+		// Sets name and gender
+
+		System.out.println("What is your name?");
+		name = sc.nextLine();
+		// character.setName(input);
+		System.out.println("What is your gender?");
+		gend = sc.nextLine();
+		// character.setGender(input);
+		while (!valid) {
+			try {
+				// selects Character type
+				System.out.println("What character class do you want to be? Input a number for each result.");
+				System.out.println("Charmer: 1");
+				System.out.println("Artist: 2");
+				System.out.println("Jock: 3");
+				System.out.println("Unspecialized: 4");
+				System.out.println("Nerd: 5");
+				num = sc.nextInt();
+				valid = true;
+			} catch (NumberFormatException nfx) {
+				System.out.println("Invalid input, please try again.")
+			}
+		}
+	}
+
+	public void chooseCourses() {
+		character.courseSelection();
+	}
+	// initialized once sufficient time has passed
+
+	// TODO: Finish ending
+	public void startEnding() {
+
+	}
 
 }
