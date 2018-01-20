de13cc8dcf81d2dcc6a018939a0426096f38390c
OrigamiRice <35274219+OrigamiRice@users.noreply.github.com>
2018-01-19 21:12:54 -0500

    Update Predetermined.java

--- Predetermined.java
+++ Predetermined.java
@@ -1,8 +1,7 @@
 package final_Project;
 
 public class Predetermined extends Event{
-	public Predetermined(String id, int statType, int statReq, int changeType, int changeAmount, int month, EventPhase[] phases){
+	public Predetermined(String id, int statType, int statReq, int changeType, int changeAmount, EventPhase[] phases){
 		super(id, statType, statReq, changeType, changeAmount, phases);	
-		this.month = month; 
 	}
 }
