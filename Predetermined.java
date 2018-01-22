861a3fd9c3aaeab8b3702fc7e49300ee725252d7
Joseph Wang <35274193+joseph001126@users.noreply.github.com>
2018-01-22 13:45:06 -0500

    Update Predetermined.java

--- Predetermined.java
+++ Predetermined.java
@@ -1,10 +1,11 @@
 public class Predetermined extends Event{
 	private int month; 
+	//Contructor
 	public Predetermined(String id, int statType, int statReq, int changeType, int changeAmount, int month, EventPhase[] phases){
 		super(id, statType, statReq, changeType, changeAmount, phases);	
 		this.month = month; 
 	}
-	
+	//accessor
 	public int getMonthReq(){
 		return month; 
 	}
