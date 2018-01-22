a8261aaac5c16c4d9f73c9a873647af0a2cca46c
Joseph Wang <35274193+joseph001126@users.noreply.github.com>
2018-01-22 13:45:28 -0500

    Update Random.java

--- Random.java
+++ Random.java
@@ -1,4 +1,5 @@
 public class Random extends Event{
+	//Constructor
 	public Random(String id, int statType, int statReq, int changeType, int changeAmount, EventPhase[] phases){
 		super(id, statType, statReq, changeType, changeAmount, phases);	
 	}
