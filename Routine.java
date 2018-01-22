c965fc0b7a10746e19c99d865e3a82e3bfcb88e0
Joseph Wang <35274193+joseph001126@users.noreply.github.com>
2018-01-22 13:45:46 -0500

    Update Routine.java

--- Routine.java
+++ Routine.java
@@ -1,4 +1,5 @@
 public class Routine extends Event{
+	//Constructor
 	public Routine(String id, int statType, int statReq, int changeType, int changeAmount, EventPhase[] phases){
 		super(id, statType, statReq, changeType, changeAmount, phases);	
 	}
